fbd295d6a320efe3a242b4ddead15e94ac9e41c2
tienmanh31121992 <tienmanh31121992@gmail.com>
2021-07-01 14:06:57 +0700

    TienManh: Edit code

--- source/KhachHang.py
+++ source/KhachHang.py
@@ -1,12 +1,12 @@
 """
     @api {post} /customers/register Đăng ký
     @apiName Đăng_ký
-    @apiGroup Tài_khoản
+    @apiGroup Khách_hàng
     @apiVersion 1.0.0
     @apiDescription Khách hàng đăng ký tài khoản
 
 
-    @apiHeader {String} Content-Type application/json
+    @apiHeader {String} Content-Type <mark>application/json</mark>
 
 
     @apiParam (Body) {String} Object.data.ten_khach_hang Tên khách hàng
@@ -14,17 +14,17 @@
     @apiParam (Body) {String} Object.data.so_dien_thoai Số điện thoại
     @apiParam (Body) {String} Object.data.mat_khau Mật khẩu
     @apiParam (Body) {String} Object.data.dia_chi Địa chỉ khách hàng
-    @apiParam (Body) {Date}   [Object.data.ngay_sinh] Ngày sinh
-    @apiParam (Body) {String} [Object.data.email] Địa chỉ thư điện tử
-    @apiParam (Body) {String} [Object.data.ten_nguoi_giam_ho] Tên người giám hộ
-    @apiParam (Body) {String} [Object.data.so_cmnd] Số chứng minh thư, căn cước, hộ chiếu
-    @apiParam (Body) {Date}   [Object.data.ngay_cap_cmnd] Ngày cấp CMT, CCCD, HC
-    @apiParam (Body) {String} [Object.data.noi_cap_cmnd] Nơi cấp CMT, CCCD, HC
+    @apiParam (Body) {Date}   Object.data.ngay_sinh Ngày sinh
+    @apiParam (Body) {String} Object.data.email Địa chỉ thư điện tử
+    @apiParam (Body) {String} Object.data.ten_nguoi_giam_ho Tên người giám hộ
+    @apiParam (Body) {String} Object.data.so_cmnd Số chứng minh thư, căn cước, hộ chiếu
+    @apiParam (Body) {Date}   Object.data.ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
+    @apiParam (Body) {String} Object.data.noi_cap_cmnd Nơi cấp CMT, CCCD, HC
     @apiParam (Body) {Number} Object.data.tinh_tp_id ID tỉnh/thành phố
     @apiParam (Body) {Number} Object.data.quan_huyen_id ID quận/huyện
     @apiParam (Body) {Number} Object.data.xa_phuong_id ID xã/phường
 
-    @apiParamExample  {JSON} JSON - Body request:
+    @apiParamExample {JSON} JSON - Body request:
     {
         "ten_khach_hang": "Tiến Mạnh",
         "so_dien_thoai": "0123456789",
@@ -43,20 +43,18 @@
     }
 
 
-    @apiSuccess (Success 2xx) 200-OK Yêu cầu được tiếp nhận và xử lý thành công
-    <ul>
-        <li><code>code:</code> 200</li>
-        <li><code>message:</code> Thông báo kết quả</li>
-    </ul>
+    @apiSuccess {String} code Mã trạng thái HTTP
+    <br><mark>200-OK: Yêu cầu được tiếp nhận và xử lý thành công</mark><br>
+    @apiSuccess {String} message Thông báo kết quả
 
-    @apiSuccessExample {JSON} 200 OK:
+    @apiSuccessExample {JSON} Success 200:
     {
         "code": 200,
         "message": "Đăng ký tài khoản thành công!"
     }
 
 
-    @apiError (Error 4xx) 412-PreconditionFailed Lỗi khi kiểm tra dữ liệu
+    @apiError 412-PreconditionFailed Lỗi kiểm tra điều kiện
     <ul>
         <li><code>code:</code> 412</li>
         <li><code>message:</code> Thông báo lỗi</li>
@@ -72,60 +70,61 @@
         "code": 412,
         "message": "Tài khoản đã tồn tại!"
     }
-
     @apiErrorExample {JSON} Error 500:
     {
         "code": 500,
-        "message": "Xảy ra lỗi khi đăng ký tài khoản!"
+        "message": "Thông báo lỗi cụ thể"
     }
 """
 
 """
-   @api {post} /customers/login Đăng nhập
-   @apiName Đăng_nhập
-   @apiGroup Tài_khoản
-   @apiVersion  1.0.0
-   @apiDescription Khách hàng đăng nhập vào hệ thống
+    @api {get} /customers/login Đăng nhập
+    @apiName Đăng_nhập
+    @apiGroup Khách_hàng
+    @apiVersion  1.0.0
+    @apiDescription Khách hàng đăng nhập vào hệ thống
 
 
-   @apiHeader {String} Content-Type application/json
+    @apiHeader {String} Content-Type <mark>application/json</mark>
 
-   
-   @apiParam (Body) {String} so_dien_thoai Số điện thoại khách hàng
-   @apiParam (Body) {String} mat_khau Mật khẩu đăng nhập
+
+    @apiParam (Body) {String} so_dien_thoai Số điện thoại khách hàng
+    @apiParam (Body) {String} mat_khau Mật khẩu đăng nhập
 
     @apiParamExample {JSON} JSON - Body request:
     {
-       "so_dien_thoai": "0123456789",
-       "mat_khau": "a1b2c3A@"
+        "so_dien_thoai": "0123456789",
+        "mat_khau": "a1b2c3A@"
     }
 
-   
-   @apiSuccess (Thành công 200) {Object} Object Kết quả trả về 
-   @apiSuccess (Thành công 200) {String} Object.code Mã trạng thái HTTP
-   @apiSuccess (Thành công 200) {String} Object.message Thông báo kết quả
-   @apiSuccess (Thành công 200) {Object} Object.data Đối tượng khách hàng
-   @apiSuccess (Thành công 200) {Number} Object.data.id ID khách hàng
-   @apiSuccess (Thành công 200) {String} Object.data.ma_khach_hang Mã khách hàng
-   @apiSuccess (Thành công 200) {String} Object.data.ten_khach_hang Tên khách hàng
-   @apiSuccess (Thành công 200) {String} Object.data.so_dien_thoai Số điện thoại
-   @apiSuccess (Thành công 200) {String} Object.data.dia_chi Địa chỉ khách hàng
-   @apiSuccess (Thành công 200) {Date}   Object.data.ngay_sinh Ngày sinh
-   @apiSuccess (Thành công 200) {String} Object.data.link_anh_dai_dien Đường dẫn lưu ảnh đại diện
-   @apiSuccess (Thành công 200) {String} Object.data.email Địa chỉ thư điện tử
-   @apiSuccess (Thành công 200) {Number} Object.data.gioi_tinh Giới tính khách hàng
-   @apiSuccess (Thành công 200) {String} Object.data.ten_nguoi_giam_ho Tên người giám hộ
-   @apiSuccess (Thành công 200) {String} Object.data.so_cmnd Số chứng minh thư, căn cước, hộ chiếu
-   @apiSuccess (Thành công 200) {Date}   Object.data.ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
-   @apiSuccess (Thành công 200) {String} Object.data.noi_cap_cmnd Nơi cấp CMT, CCCD, HC
-   @apiSuccess (Thành công 200) {Number} Object.data.tinh_tp_id ID tỉnh/thành phố
-   @apiSuccess (Thành công 200) {Number} Object.data.quan_huyen_id ID quận/huyện
-   @apiSuccess (Thành công 200) {Number} Object.data.xa_phuong_id ID xã/phường
-
-   @apiSuccessExample {JSON} Đăng nhập thành công:
+    
+    @apiSuccess {Object} data Thông tin của khách hàng
+    @apiSuccess {Number} data.id ID khách hàng
+    @apiSuccess {String} data.ma_khach_hang Mã khách hàng
+    @apiSuccess {String} data.ten_khach_hang Tên khách hàng
+    @apiSuccess {String} data.so_dien_thoai Số điện thoại
+    @apiSuccess {String} data.dia_chi Địa chỉ khách hàng
+    @apiSuccess {Date}   data.ngay_sinh Ngày sinh
+    @apiSuccess {String} data.link_anh_dai_dien Đường dẫn lưu ảnh đại diện
+    @apiSuccess {String} data.email Địa chỉ thư điện tử
+    @apiSuccess {Number} data.gioi_tinh Giới tính khách hàng
+    @apiSuccess {String} data.ten_nguoi_giam_ho Tên người giám hộ
+    @apiSuccess {String} data.so_cmnd Số chứng minh thư, căn cước, hộ chiếu
+    @apiSuccess {Date}   data.ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
+    @apiSuccess {String} data.noi_cap_cmnd Nơi cấp CMT, CCCD, HC
+    @apiSuccess {Number} data.tinh_tp_id ID tỉnh/thành phố
+    @apiSuccess {Number} data.quan_huyen_id ID quận/huyện
+    @apiSuccess {Number} data.xa_phuong_id ID xã/phường
+    @apiSuccess {String} mesage Thông báo kết quả
+    @apiSuccess {String} token Chuỗi Token
+    @apiSuccess {String} code Mã trạng thái HTTP
+    <br><mark>200-OK: Yêu cầu được tiếp nhận và xử lý thành công</mark><br>
+    
+    @apiSuccessExample {JSON} Success 200:
     {
         "code": 200,
         "message": "Đăng nhập thành công!",
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
         "data": {
             "id": 1,
             "ma_khach_hang": "A0000001",
@@ -145,50 +144,80 @@
             "xa_phuong_id": 3
         }
     }
+
    
+    @apiError (Error 4xx) 403-Forbidden Truy cập dữ liệu bị từ chối
+    <ul>
+        <li><code>code:</code> 403</li>
+        <li><code>message:</code> Thông báo lỗi</li>
+    </ul>
+    @apiError (Error 4xx) 404-NotFound Lỗi truy cập dữ liệu không tồn tại
+    <ul>
+        <li><code>code:</code> 404</li>
+        <li><code>message:</code> Thông báo lỗi</li>
+    </ul>
+    @apiError (Error 4xx) 412-PreconditionFailed Lỗi khi kiểm tra dữ liệu
+    <ul>
+        <li><code>code:</code> 412</li>
+        <li><code>message:</code> Thông báo lỗi</li>
+    </ul>
+    @apiError (Error 5xx) 500-InternalServerError Lỗi Server
+    <ul>
+        <li><code>code:</code> 500</li>
+        <li><code>message:</code> Thông báo lỗi</li>
+    </ul>
 
-   @apiError (Thất bại 400/500) {Object} Object Kết quả trả về 
-   @apiError (Thất bại 400/500) {String} Object.code Mã trạng thái HTTP
-   @apiError (Thất bại 400/500) {String} Object.message Thông báo kết quả
-
-   @apiErrorExample {JSON} Đăng nhập thất bại:
-   {
-       "code": 400,
-       "message": "Mật khẩu không đúng! | Tài khoản không tồn tại! | Tài khoản đã bị khóa!"
-   }
-
-   @apiErrorExample {JSON} Lỗi hệ thống:
-   {
-       "code": 500,
-       "message": "Xảy ra lỗi khi đăng nhập!"
-   }
+    @apiErrorExample {JSON} Error 403:
+    {
+        "code": 403,
+        "message": "Tài khoản đang bị khóa!"
+    }
+    @apiErrorExample {JSON} Error 404:
+    {
+        "code": 404,
+        "message": "Tài khoản không tồn tại!"
+    }
+    @apiErrorExample {JSON} Error 412:
+    {
+        "code": 412,
+        "message": "Mật khẩu không đúng!"
+    }
+    @apiErrorExample {JSON} Lỗi hệ thống:
+    {
+        "code": 500,
+        "message": "Xảy ra lỗi khi đăng nhập!"
+    }
 """
 
 """
-    @api {put} /khach-hang/cap-nhat-thong-tin Cập nhật thông tin
+    @api {put} /customers/update-info Cập nhật thông tin
     @apiName Cập_nhật_thông_tin
-    @apiGroup Tài_khoản
+    @apiGroup Khách_hàng
     @apiVersion 1.0.0
     @apiDescription Khách hàng cập nhật thông tin tài khoản
 
-    @apiParam {Object} Object Đối tượng khách hàng
-    @apiParam {String} Object.data.id ID khách hàng
-    @apiParam {String} Object.data.ten_khach_hang Tên khách hàng
-    @apiParam {String} Object.data.so_dien_thoai Số điện thoại
-    @apiParam {String} Object.data.dia_chi Địa chỉ khách hàng
-    @apiParam {Date}   Object.data.ngay_sinh Ngày sinh
-    @apiParam {String} Object.data.link_anh_dai_dien Đường dẫn lưu ảnh đại diện
-    @apiParam {String} Object.data.email Địa chỉ thư điện tử
-    @apiParam {Number} Object.data.gioi_tinh Giới tính khách hàng
-    @apiParam {String} Object.data.ten_nguoi_giam_ho Tên người giám hộ
-    @apiParam {String} Object.data.so_cmnd Số chứng minh thư, căn cước, hộ chiếu
-    @apiParam {Date}   Object.data.ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
-    @apiParam {String} Object.data.noi_cap_cmnd Nơi cấp CMT, CCCD, HC
-    @apiParam {Number} Object.data.tinh_tp_id ID tỉnh/thành phố
-    @apiParam {Number} Object.data.quan_huyen_id ID quận/huyện
-    @apiParam {Number} Object.data.xa_phuong_id ID xã/phường
+    
+    @apiHeader {String} Content-Type <mark>application/json</mark>
+    @apiHeader {String} Authorization <code>Bearer</code> <mark>Chuỗi Token</mark>
 
-    @apiParamExample  {JSON} Dữ liệu mẫu:
+
+    @apiParam (Body) {Number} id ID khách hàng
+    @apiParam (Body) {String} ten_khach_hang Tên khách hàng
+    @apiParam (Body) {String} so_dien_thoai Số điện thoại
+    @apiParam (Body) {String} dia_chi Địa chỉ khách hàng
+    @apiParam (Body) {Date}   ngay_sinh Ngày sinh
+    @apiParam (Body) {String} link_anh_dai_dien Đường dẫn lưu ảnh đại diện
+    @apiParam (Body) {String} email Địa chỉ thư điện tử
+    @apiParam (Body) {Number} gioi_tinh Giới tính khách hàng
+    @apiParam (Body) {String} ten_nguoi_giam_ho Tên người giám hộ
+    @apiParam (Body) {String} so_cmnd Số chứng minh thư, căn cước, hộ chiếu
+    @apiParam (Body) {Date}   ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
+    @apiParam (Body) {String} noi_cap_cmnd Nơi cấp CMT, CCCD, HC
+    @apiParam (Body) {Number} tinh_tp_id ID tỉnh/thành phố
+    @apiParam (Body) {Number} quan_huyen_id ID quận/huyện
+    @apiParam (Body) {Number} xa_phuong_id ID xã/phường
+
+    @apiParamExample {JSON} JSON - Body request:
     {
         "id": 1,
         "ten_khach_hang": "Phạm Tiến Mạnh",
@@ -208,26 +237,26 @@
     }
 
 
-    @apiSuccess (Thành công 200) {Object} Object Kết quả trả về 
-    @apiSuccess (Thành công 200) {String} Object.code Mã trạng thái HTTP
-    @apiSuccess (Thành công 200) {String} Object.message Thông báo kết quả
-    @apiSuccess (Thành công 200) {Object} Object.data Đối tượng khách hàng
-    @apiSuccess (Thành công 200) {Number} Object.data.id ID khách hàng
-    @apiSuccess (Thành công 200) {String} Object.data.ma_khach_hang Mã khách hàng
-    @apiSuccess (Thành công 200) {String} Object.data.ten_khach_hang Tên khách hàng
-    @apiSuccess (Thành công 200) {String} Object.data.so_dien_thoai Số điện thoại
-    @apiSuccess (Thành công 200) {String} Object.data.dia_chi Địa chỉ khách hàng
-    @apiSuccess (Thành công 200) {Date}   Object.data.ngay_sinh Ngày sinh
-    @apiSuccess (Thành công 200) {String} Object.data.link_anh_dai_dien Đường dẫn lưu ảnh đại diện
-    @apiSuccess (Thành công 200) {String} Object.data.email Địa chỉ thư điện tử
-    @apiSuccess (Thành công 200) {Number} Object.data.gioi_tinh Giới tính khách hàng
-    @apiSuccess (Thành công 200) {String} Object.data.ten_nguoi_giam_ho Tên người giám hộ
-    @apiSuccess (Thành công 200) {String} Object.data.so_cmnd Số chứng minh thư, căn cước, hộ chiếu
-    @apiSuccess (Thành công 200) {Date}   Object.data.ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
-    @apiSuccess (Thành công 200) {String} Object.data.noi_cap_cmnd Nơi cấp CMT, CCCD, HC
-    @apiSuccess (Thành công 200) {Number} Object.data.tinh_tp_id ID tỉnh/thành phố
-    @apiSuccess (Thành công 200) {Number} Object.data.quan_huyen_id ID quận/huyện
-    @apiSuccess (Thành công 200) {Number} Object.data.xa_phuong_id ID xã/phường
+    @apiSuccess {Object} Object Kết quả trả về 
+    @apiSuccess {String} Object.code Mã trạng thái HTTP
+    @apiSuccess {String} Object.message Thông báo kết quả
+    @apiSuccess {Object} Object.data Đối tượng khách hàng
+    @apiSuccess {Number} Object.data.id ID khách hàng
+    @apiSuccess {String} Object.data.ma_khach_hang Mã khách hàng
+    @apiSuccess {String} Object.data.ten_khach_hang Tên khách hàng
+    @apiSuccess {String} Object.data.so_dien_thoai Số điện thoại
+    @apiSuccess {String} Object.data.dia_chi Địa chỉ khách hàng
+    @apiSuccess {Date}   Object.data.ngay_sinh Ngày sinh
+    @apiSuccess {String} Object.data.link_anh_dai_dien Đường dẫn lưu ảnh đại diện
+    @apiSuccess {String} Object.data.email Địa chỉ thư điện tử
+    @apiSuccess {Number} Object.data.gioi_tinh Giới tính khách hàng
+    @apiSuccess {String} Object.data.ten_nguoi_giam_ho Tên người giám hộ
+    @apiSuccess {String} Object.data.so_cmnd Số chứng minh thư, căn cước, hộ chiếu
+    @apiSuccess {Date}   Object.data.ngay_cap_cmnd Ngày cấp CMT, CCCD, HC
+    @apiSuccess {String} Object.data.noi_cap_cmnd Nơi cấp CMT, CCCD, HC
+    @apiSuccess {Number} Object.data.tinh_tp_id ID tỉnh/thành phố
+    @apiSuccess {Number} Object.data.quan_huyen_id ID quận/huyện
+    @apiSuccess {Number} Object.data.xa_phuong_id ID xã/phường
 
     @apiSuccessExample {JSON} Cập nhật thông tin thành công:
     {
@@ -274,7 +303,7 @@
 """
     @api {put} /khach-hang/doi-mat-khau Đổi mật khẩu
     @apiName Đổi_mật_khẩu
-    @apiGroup Tài_khoản
+    @apiGroup Khách_hàng
     @apiVersion 1.0.0
     @apiDescription Khách hàng thay đổi mật khẩu
 
@@ -293,9 +322,9 @@
     }
 
 
-    @apiSuccess (Thành công 200) {Object} Object Kết quả trả về 
-    @apiSuccess (Thành công 200) {String} Object.code Mã trạng thái HTTP
-    @apiSuccess (Thành công 200) {String} Object.message Thông báo kết quả
+    @apiSuccess {Object} Object Kết quả trả về 
+    @apiSuccess {String} Object.code Mã trạng thái HTTP
+    @apiSuccess {String} Object.message Thông báo kết quả
 
     @apiSuccessExample {JSON} Đổi mật khẩu thành công:
     {
